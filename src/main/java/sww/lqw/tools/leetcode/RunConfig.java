8d2747378c4c914f6282b4b14e7448f875607063
quanwei.lqw <quanwei.lqw@alibaba-inc.com>
2015-06-20 21:27:46 +0800

    add file RunConfig.java

---
+++ src/main/java/sww/lqw/tools/leetcode/RunConfig.java
@@ -0,0 +1,43 @@
+package sww.lqw.tools.leetcode;
+
+public class RunConfig {
+	/**
+	 * leetcode username
+	 */
+	private String username;
+	
+	/**
+	 * leetcode password
+	 */
+	private String password;
+	
+	/**
+	 * git clone url
+	 */
+	private String cloneUrl;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCloneUrl() {
+		return cloneUrl;
+	}
+
+	public void setCloneUrl(String cloneUrl) {
+		this.cloneUrl = cloneUrl;
+	}
+	
+}
