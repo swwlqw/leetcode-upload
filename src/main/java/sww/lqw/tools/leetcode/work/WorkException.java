24d9abb4cea5c0a707f44f2e3f77b4b58143e536
swwlqw <swwefj@qq.com>
2015-06-22 00:29:29 +0800

    Add WorkException

---
+++ src/main/java/sww/lqw/tools/leetcode/work/WorkException.java
@@ -0,0 +1,23 @@
+package sww.lqw.tools.leetcode.work;
+
+public class WorkException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public WorkException() {
+		super();
+	}
+
+	public WorkException(String message) {
+		super(message);
+	}
+
+	public WorkException(Throwable cause) {
+		super(cause);
+	}
+
+	public WorkException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
