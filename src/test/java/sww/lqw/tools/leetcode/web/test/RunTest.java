2bac47d9a80de52c1a4393f017ca1d80403500ea
swwlqw <swwefj@qq.com>
2015-06-22 01:15:12 +0800

    Add RunTest

---
+++ src/test/java/sww/lqw/tools/leetcode/web/test/RunTest.java
@@ -0,0 +1,23 @@
+package sww.lqw.tools.leetcode.web.test;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+import sww.lqw.tools.leetcode.Main;
+import sww.lqw.tools.leetcode.work.Works;
+
+public class RunTest {
+
+	@Test
+	public void testRun() throws Exception {
+		Class<?> clz = Main.class;
+		Method load = clz.getDeclaredMethod("loadConfig");
+		load.setAccessible(true);
+		load.invoke(null);
+		String[] tasks = new String[] { "login", "acceptList" };
+		for (String task : tasks) {
+			Works.getWorkByCommand(task).run();
+		}
+	}
+}
