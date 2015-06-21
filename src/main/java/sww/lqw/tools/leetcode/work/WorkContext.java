63638daaec4d21957dec6444c3df93de59048c9c
swwlqw <swwefj@qq.com>
2015-06-21 23:44:59 +0800

    make context final

--- src/main/java/sww/lqw/tools/leetcode/work/WorkContext.java
+++ src/main/java/sww/lqw/tools/leetcode/work/WorkContext.java
@@ -2,14 +2,10 @@ package sww.lqw.tools.leetcode.work;
 
 public class WorkContext {
 
-	private static WorkContext context = new WorkContext();
+	private static final WorkContext context = new WorkContext();
 
 	public static WorkContext getContext() {
 		return context;
-	}
-
-	public static void setContext(WorkContext context) {
-		WorkContext.context = context;
 	}
 
 	private WorkContext() {
