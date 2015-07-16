1afe68de61f90436c4ea72c5ee2cb5136cebb6d0
swwlqw <swwefj@qq.com>
2015-07-16 21:50:37 +0800

    Add Const.java

---
+++ src/main/java/sww/lqw/tools/leetcode/Const.java
@@ -0,0 +1,9 @@
+package sww.lqw.tools.leetcode;
+
+public interface Const {
+	
+	String COMMIT_MESSAGE = "Auto commit by https://github.com/swwlqw/leetcode-upload";
+	
+	String PROBLEM_DIR = "problems";
+	
+}
